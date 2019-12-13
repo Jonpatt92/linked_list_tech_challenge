c66297c3654ab34715361b64cc22036f21850f7e
Jon Patterson <JonPatt92@gmail.com>
2019-12-13 11:10:22 -0700

    Add find_tail method to iteratively return the tail node. Refactor all other methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -11,49 +11,40 @@ class LinkedList
     if head == nil
       @head = Node.new(surname)
     else
-      @head.next_node = Node.new(surname)
+      node = find_tail
+      node.add_node(surname)
     end
   end
 
   def count
     node_count = 0
-    return node_count if !head
-
-    if !head.next_node
-      return node_count += 1
-    else
-      count_node_list
+    node = head
+    until !node
+      node = node.next_node
+      node_count +=1
     end
+    node_count
   end
 
   def to_string
     if !head
       "The Families have all perished"
-    elsif !head.next_node
-      "The #{head.surname} Family"
     else
-      gather_families
+      string = "The #{head.surname} Family"
+      node = head.next_node
+      until !node
+        string += ", followed by the #{node.surname} family"
+        node = node.next_node
+      end
+      string
     end
   end
 
-  def gather_families
-    families = []
-    node = head.next_node
+  def find_tail
+    node = head
     until !node.next_node
-      families << node.surname + ', '
       node = node.next_node
     end
-    families.last.delete! ', '
-    "The #{families.flatten} Families"
-  end
-
-  def count_node_list
-    node_count = 2
-    node = head.next_node
-    until !node.next_node
-      node_count += 1
-      node = node.next_node
-    end
-    node_count
+    node
   end
 end
