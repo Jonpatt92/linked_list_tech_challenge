0902e03200d2724a642adf2568a0cfa2cff3a010
Jon Patterson <JonPatt92@gmail.com>
2019-12-13 09:38:30 -0700

    Create node test for iteration 0

--- test/node_test.rb
+++ test/node_test.rb
@@ -1,8 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
 require './lib/node.rb'
 
 class NodeTest < Minitest::Test
 
   def setup
+    @node = Node.new("Burke")
+  end
+
+  def test_node_exists
+    assert_instance_of Node, @node
+  end
+
+  def test_it_has_attributes
+    assert_nil @node.next_node
+    assert_equal "Burke", @node.surname
   end
 
 end
