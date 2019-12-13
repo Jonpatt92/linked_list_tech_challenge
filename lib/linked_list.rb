774c14a80edb16b439c7b4385761c75e30e78ee8
Jon Patterson <JonPatt92@gmail.com>
2019-12-13 12:31:51 -0700

    Add find Find_position, insert, and find needs to be refactored to reduce technical debt

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -27,16 +27,12 @@ class LinkedList
       head.set_next_node(previous_head)
     end
   end
-  
+
   def insert(position, surname, node = head)
-    new_node = Node.new(surname)
-    (position - 1).times do
-      unless !node.next_node
-        node = node.next_node
-      end
-    end
-    continuing_node = node.next_node
-    inserted_node = node.set_next_node(new_node)
+    new_node        = Node.new(surname)
+    positional_node = find_position(position)
+    continuing_node = positional_node.next_node
+    inserted_node   = positional_node.set_next_node(new_node)
     inserted_node.set_next_node(continuing_node)
   end
 
@@ -48,18 +44,33 @@ class LinkedList
     end
   end
 
-  def to_string
+  def to_string(head_node = head, count = nil)
     if !head
       "The Families have all perished"
     else
-      string = "The #{head.surname} Family"
-      node = head.next_node
-      until !node
+      string = "The #{head_node.surname} family"
+      node = head_node.next_node
+      until !node || count == 0
         string += ", followed by the #{node.surname} family"
         node = node.next_node
+        count -= 1 if count
       end
       string
     end
+  end
+
+  def find(position, elements)
+    positional_node = find_position(position + 1)
+    to_string(positional_node, elements)
+  end
+
+  def find_position(position, node = head)
+    (position - 1).times do
+      unless !node.next_node
+        node = node.next_node
+      end
+    end
+    node
   end
 
   def find_tail(node = head)
